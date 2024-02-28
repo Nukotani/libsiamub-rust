d6c8815743bd27e4bac28c68a282643c45cee6f2
nukotani <fatihsatrio121@gmail.com>
2024-02-28 20:47:40 +0700

    implemented attendance parser(slow af)

--- src/attendace.rs
+++ src/attendace.rs
@@ -1,6 +1,6 @@
-use curl::easy::Easy;
+use scraper::{Html, Selector};
 
-use crate::session::{self, Session};
+use crate::session::Session;
 
 
 const URL: &str = "https://siam.ub.ac.id/absensi.php";
@@ -17,17 +17,52 @@ impl AttendanceList {
         }
     }
 
-    pub fn fetch_list(&mut self, session: &Session, handle: &mut Easy) {
-        handle.url(URL).unwrap();
-        handle.get(true).unwrap();
-        handle.cookie(session.get_id()).unwrap();
-        
-        {
-            let mut transfer = handle.transfer();
+    //TODO: OPTIMIZE!
+    pub fn fetch_list(&mut self, session: &mut Session) {
+        session.connect(URL);
 
-            transfer.write_function(|body|
-                Ok(body.len())
-            ).unwrap();
+        let mut buf = String::new();
+        session.write_function(|body| {
+            buf = String::from_utf8(body.to_vec()).unwrap();
+            Ok(body.len())
+        });
+        
+    }
+
+    fn parse_body(body: &str) {
+        let document = Html::parse_document(&body);
+        let selector = Selector::parse("table.table-bordered").unwrap();
+
+        let table = document.select(&selector).next().unwrap();
+        let inner_table = table.first_child().unwrap().first_child().unwrap()
+            .first_child().unwrap();
+        let mut inner_table_iterator = inner_table.children();
+        inner_table_iterator.next();
+
+        for entry in inner_table_iterator {
+            let mut entry_iterator = entry.children();
+            let code: String = entry_iterator.next().unwrap().first_child().unwrap().value()
+                .as_text().unwrap().to_string();
+
+            let name: String = entry_iterator.next().unwrap().first_child().unwrap().value()
+                .as_text().unwrap().to_string();
+
+            let class: String = entry_iterator.next().unwrap().first_child().unwrap().value()
+                .as_text().unwrap().to_string();
+
+            let present: u32 = entry_iterator.next().unwrap().first_child().unwrap().value()
+                .as_text().unwrap().to_string().parse::<u32>().unwrap();
+
+            let unexcused: u32 = entry_iterator.next().unwrap().first_child().unwrap().value()
+                .as_text().unwrap().to_string().parse::<u32>().unwrap();
+
+            let excused: u32 = entry_iterator.next().unwrap().first_child().unwrap().value()
+                .as_text().unwrap().to_string().parse::<u32>().unwrap();
+
+            let medical: u32 = entry_iterator.next().unwrap().first_child().unwrap().value()
+                .as_text().unwrap().to_string().parse::<u32>().unwrap();
+
+            let stat = AttendanceStat::new(present, unexcused, excused, medical);
         }
     }
 }
@@ -40,9 +75,52 @@ pub struct AttendanceEntry {
     details: Option<AttendanceDetail>,
 }
 
-struct AttendanceStat {
+impl AttendanceEntry {
+    pub fn new(code: &str, name: &str, class: &str, stat: AttendanceStat) -> AttendanceEntry {
+        let code = code.to_string();
+        let name = name.to_string();
+        let class = class.to_string();
+        let details = None;
+        let stat = stat;
 
+        AttendanceEntry {
+            code,
+            name,
+            class,
+            details,
+            stat,
+        }
+    }
+} 
+
+struct AttendanceStat {
+    present: u32,
+    unexcused: u32,
+    excused: u32,
+    medical: u32,
+}
+
+impl AttendanceStat {
+    pub fn new(present: u32, unexcused: u32, excused: u32, medical: u32) -> AttendanceStat {
+        AttendanceStat {
+            present,
+            unexcused,
+            excused,
+            medical,
+        }
+    }
 }
 
 struct AttendanceDetail {
+    lesson: String,
+    //TODO: use a more suited type!
+    note: String,
+    status: AttendanceStatus,
+}
+
+enum AttendanceStatus {
+    Present,
+    Unexcused,
+    Excused,
+    Medical,
 }
--- src/session.rs
+++ src/session.rs
@@ -1,4 +1,6 @@
-use curl::easy::{Easy, Form};
+use std::{default, str::Bytes};
+
+use curl::easy::{Easy, Form, WriteError};
 
 const URL: &str = "https://siam.ub.ac.id/index.php";
 const USER_AGENT: &str = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36";
@@ -20,9 +22,8 @@ impl Session {
         }
     }
 
-    pub fn login(&mut self, id: &str, pass: &str, handle: &mut Easy) {
-        handle.url(URL).unwrap();
-
+    //TODO: OPTIOMIZE! 
+    pub fn login(&mut self, id: &str, pass: &str) {
         let mut form = Form::new();
         form.part("status_loc").contents("success".as_bytes()).add().unwrap();
         form.part("lat").contents("0".as_bytes()).add().unwrap();
@@ -31,17 +32,22 @@ impl Session {
         form.part("password").contents(pass.as_bytes()).add().unwrap();
         form.part("login").contents("masuk.".as_bytes()).add().unwrap();
 
-        handle.post(true).unwrap();
-        handle.httppost(form).unwrap();
+        self.handle.url(URL).unwrap();
+        self.handle.post(true).unwrap();
+        self.handle.httppost(form).unwrap();
 
+        let mut buf: String = Default::default();
         {
-            let mut transfer = handle.transfer();
+            let mut transfer = self.handle.transfer();
+
             transfer.header_function(|header| {
-                self.set_id_from_header(header);
+                buf = String::from_utf8(header.to_vec()).unwrap();
                 true
             }).unwrap();
             transfer.perform().unwrap();
         }
+
+        self.set_id_from_header(buf.as_bytes());
     }
 
     //TODO: OPTIMIZE!
@@ -61,6 +67,21 @@ impl Session {
 
     pub fn get_id(&self) -> &str {
         self.id.as_str()
+    }
+
+    pub fn connect(&mut self, url: &str) {
+        self.handle.cookie(self.id.as_str()).unwrap();
+        self.handle.url(url).unwrap();
+        self.handle.get(true).unwrap();
+    }
+
+    pub fn write_function<F>(&mut self, f: F)
+    where
+        F: FnMut(&[u8]) -> Result<usize, WriteError>,
+    {
+        let mut transfer = self.handle.transfer(); 
+
+        transfer.write_function(f).unwrap();
     }
 }
 
